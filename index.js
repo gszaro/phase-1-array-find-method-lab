94b56ffa38149e84254e085775589d80cfa02220
Gregory Szaro <gregoryszaro@gmail.com>
2023-05-02 16:41:16 -0400

    completes find() lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function superbowlWin(record) {
+  const winRecord = record.find(game => game.result === "W");
+  if (winRecord) {
+    return winRecord.year;
+  } else {
+    return undefined;
+  }
+}
+
+const denverBroncosRecord = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"}
+];
+
+console.log(superbowlWin(denverBroncosRecord));
